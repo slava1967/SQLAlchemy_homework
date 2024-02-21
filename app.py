57106418757855f439a1d17acadde5158dfea5f7
Viacheslav <fx.slav21@gmail.com>
2024-02-21 14:43:26 +0300

    added 2 endpoints and 2 templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from sqlalchemy import desc
 
 from database import Book, Genre, db
@@ -14,7 +14,21 @@ def all_books():
     return render_template("all_books.html", books=books)
 
 
-@app.route("/<int:book_id>", methods=["GET", "POST"])
+@app.route("/book/status", methods=["GET", "POST"])
+def get_book_id():
+    if request.method == 'POST':
+        book_id = int(request.form["book_id"])
+        return redirect(url_for('result', book_id=book_id))
+    return render_template("status.html")
+
+
+@app.route("/book/<int:book_id>/status", methods=["GET"])
+def result(book_id):
+    book = db.get_or_404(Book, book_id)
+    return render_template("result.html", book=book)
+
+
+@app.route("/book/<int:book_id>", methods=["GET", "POST"])
 def change_status(book_id):
     book = db.get_or_404(Book, book_id)
     if request.method == "POST":
